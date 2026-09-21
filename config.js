f0ca0a134c3009bf916411ff1c4e20ef10a6bebc
r9pjww7rcs-beep <r9pjww7rcs@privaterelay.appleid.com>
2026-09-21 18:34:08 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,40 @@
+module.exports = {
+
+STORE_NAME: "NZstore",
+
+
+OWNER: [
+"6285828169882"
+],
+
+
+ADMIN: [
+"6285828169882"
+],
+
+
+PAYMENT: {
+
+METHOD: "GoPay",
+
+NUMBER: "085828626140",
+
+NAME: "Zhraa"
+
+},
+
+
+PREFIX: "/",
+
+
+SETTING: {
+
+AUTO_WELCOME: true,
+
+AUTO_RANKING: true,
+
+AUTO_EXPIRED: true
+
+}
+
+}
